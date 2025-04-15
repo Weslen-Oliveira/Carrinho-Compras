098f66e48e617f7e2dbff1b1d770249401b4dc1f
Weslen Oliveira <160598405+Weslen-Oliveira@users.noreply.github.com>
2025-04-15 11:57:23 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const produto = document.getElementById('produto').value;
+const acessorios = document.getElementById.value;
+const quantidade = document.getElementById('quantidade').value;
+const resultado = document.getElementsByTagName('p');
+
+
+const botaoAdicionar = document.getElementsByClassName('botao-adicionar')
+const botaoLimpar = document.getElementsByClassName('botao-limpar')
+
+function adicionar(){
+    let valorAtual = Number(produto.value) + Number(quantidade.value)
+    let resultado = valorAtual;
+    
+}
+
+console.log(produto);
