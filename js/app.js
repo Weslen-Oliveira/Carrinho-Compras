5d3b01c6ce48757579cf130ef66efd177a3c1b82
Weslen Oliveira <160598405+Weslen-Oliveira@users.noreply.github.com>
2025-04-17 11:15:03 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,41 @@
-const produto = document.getElementById('produto').value;
-const acessorios = document.getElementById.value;
-const quantidade = document.getElementById('quantidade').value;
-const resultado = document.getElementsByTagName('p');
-
-
-const botaoAdicionar = document.getElementsByClassName('botao-adicionar')
-const botaoLimpar = document.getElementsByClassName('botao-limpar')
+let totalGeral = 0;
 
 function adicionar(){
-    let valorAtual = Number(produto.value) + Number(quantidade.value)
-    let resultado = valorAtual;
-    
+    const produto = document.getElementById('produto').value;
+    const quantidade = document.getElementById('quantidade').value;
+    const nomeProduto = produto.split('-')[0].trim();
+    const precoProduto = produto.split('R$')[1];
+    const total = document.getElementById('valor-total');
+    const listaProdutos = document.getElementById('lista-produtos');
+
+    if(quantidade == ''){
+        alert('Quantidade nao pode estar vazia');
+        return;
+    }
+    if(quantidade  < 0 ){
+        alert('A quantidade não pode ser negativa');
+        return
+    }
+    if(quantidade == 0 ){
+        alert('A quantidade não pode ser Zero');
+        return
+    }
+
+    listaProdutos.innerHTML = listaProdutos.innerHTML +
+        `<section class="carrinho__produtos__produto">
+            <span class="texto-azul">${quantidade}x </span>${nomeProduto}<span class="texto-azul"> R$${precoProduto}</span>
+        </section>`;
+
+        let preco = quantidade * precoProduto;
+        totalGeral += preco;
+
+    total.textContent = `R$ ${totalGeral}`;
+
 }
 
-console.log(produto);
+function limpar(){
+    document.getElementById('quantidade').value = '';
+    document.getElementById('valor-total').textContent = '';
+    document.getElementById('lista-produtos').innerHTML = '';
+    totalGeral= 0;
+}
